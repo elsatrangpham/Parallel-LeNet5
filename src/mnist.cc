9a968f7d61f82f3e800819651f4bd46c0e63272f
elsatrangpham <phamhiendoantrang@gmail.com>
2024-01-03 17:12:14 +0700

    cmt

--- src/mnist.cc
+++ src/mnist.cc
@@ -60,8 +60,8 @@ void MNIST::read_mnist_label(std::string filename, Matrix& labels) {
 
 void MNIST::read() {
   std::cout << data_dir << std::endl;
-  read_mnist_data(data_dir + "train-images-idx3-ubyte.gz", train_data);
-  read_mnist_data(data_dir + "t10k-images-idx3-ubyte.gz", test_data);
-  read_mnist_label(data_dir + "train-labels-idx1-ubyte.gz", train_labels);
-  read_mnist_label(data_dir + "t10k-labels-idx1-ubyte.gz", test_labels);
+  read_mnist_data(data_dir + "train-images-idx3-ubyte", train_data);
+  read_mnist_data(data_dir + "t10k-images-idx3-ubyte", test_data);
+  read_mnist_label(data_dir + "train-labels-idx1-ubyte", train_labels);
+  read_mnist_label(data_dir + "t10k-labels-idx1-ubyte", test_labels);
 }
